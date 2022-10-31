519802c6d4333c17dd5dce5cfe1a265be416cdde
dev-mehrab <devmehrab71@gmail.com>
2022-10-31 11:19:45 +0600

    Smart Watch Shop Fist commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Cart from "./components/cart/Cart";
+import Home from "./components/home/Home";
+import { Routes, Route} from "react-router-dom"
+import Nav from "./components/nav/Nav";
+import NotFound from "./components/NotFound";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="*" element={<NotFound/>} />
+
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+    return ( <div className="not-found">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </div> );
+}
+ 
+export default NotFound;
